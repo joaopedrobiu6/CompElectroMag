7fd3f7acf026251962b407a2c6604565368cb0a4
João Biu <joaopedrofbiu@tecnico.ulisboa.pt>
2024-11-22 14:48:07 +0100

    fff

--- lectures/SquarePulse.py
+++ lectures/SquarePulse.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 a = 1 # length simulation box
-pulse_width = 0.2
+pulse_width = 0.05
 nx = 50 # number of grid points
 dx = a/nx # grid spacing
-dt = 0.02 # time step
+dt = (1)*dx # time step
 c = 1 # speed of light in vacuum
 
 # number of time steps
@@ -18,9 +18,13 @@ Er0 = np.zeros(nx)
 Er1 = np.zeros(nx)
 Er  = np.zeros(nx)
 
-# Multiplication of true false makes and 
+# # Multiplication of true false makes and 
 Er0[(xn>(a/2 - pulse_width/2)) * (xn < (a/2 + pulse_width/2))] = 1.0
 Er1[(xn>(a/2 - pulse_width/2 + c*dt)) * (xn < (a/2 + pulse_width/2 + c*dt))] = 1.0
+
+# Gaussian pulse
+# Er0 = np.exp(-((xn - 0.5)**2)/(2*pulse_width**2))
+# Er1 = np.exp(-((xn - 0.5 + c*dt)**2)/(2*pulse_width**2))
 
 for it in range(1,nt):
   # bc 
@@ -31,6 +35,3 @@ for it in range(1,nt):
 
   Er0[:] = Er1[:]
   Er1[:] = Er[:]
-    
-plt.plot(Er)
-plt.show()
---
+++ pro2/Wave1D.py
@@ -0,0 +1,85 @@
+import numpy as np
+import scipy as sp
+import math as math
+import matplotlib.pyplot as plt
+
+#  --------------------------------------------------------------
+#  Time step 1D wave equation using two time-levels f0 & f1
+#  --------------------------------------------------------------
+#  function [omega, s1, s2] = Wave1D(a, time, nx)
+# 
+#  Arguments:
+#     a     = the length of the interval
+#     time  = the total time interval for the simulation
+#     nx    = the number of subintervals in the domain (0,a)
+#  Returns:
+#     omega = the angular frequencies
+#     s1    = the complex Fourier transform of data at x = a/5
+#     s2    = the complex Fourier transform of data at x = a/2
+
+def Wave1D(a, time, nx):
+    f0         = np.random.randn(nx) # Initialize with random numbers
+    f0[0]    = 0              # Boundary condition at x = 0
+    f0[nx-1]   = 0              # Boundary condition at x = a
+
+    f1         = np.random.randn(nx) # Initialize with random numbers
+    f1[0]    = 0              # Boundary condition at x = 0
+    f1[nx-1]   = 0              # Boundary condition at x = a
+
+    dx         = a/nx           # The cell size 
+    d2tmax     = 1.99*dx         # The time step must satisfy
+                                 # 2*dt < 2*dx for stability
+
+    ntime = np.int64(time/d2tmax + 1)  # The number of time steps
+    dt = time/(2*ntime)             # The time step
+
+    # Initialize the coefficient matrix for updating the solution f
+
+    A = np.zeros((nx,nx))
+    for i in range(1,nx-1):
+      A[i,i]   = 2*(1-(dt/dx)**2)    # Diagonal entries
+      A[i,i+1] = (dt/dx)**2          # Upper diagonal entries
+      A[i,i-1] = (dt/dx)**2          # Lower diagonal entries
+
+
+
+    sign1 = np.zeros(2*ntime)
+    sign2 = np.zeros(2*ntime)
+
+    # Time step and sample the solution
+    # Sample location #1 is close to the left boundary
+    # Sample location #2 is at the midpoint of the domain
+    for itime in range(0,ntime): # Every 'itime' means two time steps 'dt'
+
+      f0               = A@f1 - f0          # Update
+      sign1[2*itime-1] = f0[np.int64(1+nx/5)]  # Sample at location #1
+      sign2[2*itime-1] = f0[np.int64(1+nx/2)]  # Sample at location #2
+
+
+      f1               = A@f0 - f1          # Update               
+      sign1[2*itime]   = f1[np.int64(1+nx/5)]  # Sample at location #1
+      sign2[2*itime]   = f1[np.int64(1+nx/2)]  # Sample at location #2
+
+
+    # Compute the discrete Fourier transform of 
+    # the time-domain signals
+    spectr1     = np.fft.fft(sign1) 
+    spectr2     = np.fft.fft(sign2)
+
+    # In the MATLAB implementation of the function fft(), 
+    # the first half of the output corresponds to positive frequency
+    s1 = spectr1[0:ntime] 
+    s2 = spectr2[0:ntime]
+
+    # Frequency vector for use with 's1' and 's2'
+    omega       = (2*np.pi/time)*np.linspace(0, ntime-1, ntime)
+    return omega, s1, s2
+
+def main():
+    omega, s1, s2 = Wave1D(math.pi,math.pi*31,40)
+
+    plt.plot(omega, abs(s1), "b")
+    plt.show()
+
+if __name__  == "__main__":
+    main()
